971889e088d7ee8573f483d51d43e2fea55b7008
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-02-14 01:21:26 +0500

    CORS Resolve

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-
   await app.listen(5000);
 }
 bootstrap();
