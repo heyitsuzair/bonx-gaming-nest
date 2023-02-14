cb0cd030173a98949b7b18ef18b40d51df5bdcff
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-02-15 01:40:37 +0500

    API To Serve Files To Frontend

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,12 @@ import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { env } from 'config';
 import { GamesModule } from './games/games.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [AuthModule, MongooseModule.forRoot(env.database.url), GamesModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.use(express.static(__dirname));
+
   await app.listen(5000);
 }
 bootstrap();
