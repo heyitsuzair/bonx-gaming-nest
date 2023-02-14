163795ed63bccf95eb40eca46ed83129c4b717e9
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-02-14 07:24:41 +0500

    Game Model Timestamps Added

--- src/games/model/index.ts
+++ src/games/model/index.ts
@@ -1,55 +1,58 @@
 import * as mongoose from 'mongoose';
-export const GamesModel = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  short_description: {
-    type: String,
-    required: true,
-  },
-  long_description: {
-    type: String,
-    required: true,
-  },
-  features: {
-    type: String,
-    required: true,
-  },
-  whats_new: {
-    type: String,
-  },
-  installs: {
-    type: String,
-    required: true,
-  },
-  current_version: {
-    type: String,
-    required: true,
-  },
-  category: {
-    type: String,
-    required: true,
-  },
-  game_file: {
-    type: {
-      filename: {
-        type: String,
-        required: true,
-      },
-      size: {
-        type: Number,
-        required: true,
-      },
+export const GamesModel = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
     },
-    required: true,
+    short_description: {
+      type: String,
+      required: true,
+    },
+    long_description: {
+      type: String,
+      required: true,
+    },
+    features: {
+      type: String,
+      required: true,
+    },
+    whats_new: {
+      type: String,
+    },
+    installs: {
+      type: String,
+      required: true,
+    },
+    current_version: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    game_file: {
+      type: {
+        filename: {
+          type: String,
+          required: true,
+        },
+        size: {
+          type: Number,
+          required: true,
+        },
+      },
+      required: true,
+    },
+    banner: {
+      type: String,
+      required: true,
+    },
+    owner_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
   },
-  banner: {
-    type: String,
-    required: true,
-  },
-  owner_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-  },
-});
+  { timestamps: true },
+);
