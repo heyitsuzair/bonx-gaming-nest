f1f0dba2c305eddb4669d98aa510a54a8fb76bc4
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-02-15 07:55:52 +0500

    Cloudinary Inetegrated

--- utils/uploadPicture.ts
+++ utils/uploadPicture.ts
@@ -1,4 +1,6 @@
 import * as cloudinary from 'cloudinary';
+import { config } from 'dotenv';
+config();
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
